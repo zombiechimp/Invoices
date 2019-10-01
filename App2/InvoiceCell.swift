1754e5eea258bf9752d59d522aa80285227c3631
Tea Jakić <teajakic@Teas-MacBook-Pro-2.local>
2019-10-01 21:07:29 +0200

    Update

---
+++ App2/InvoiceCell.swift
@@ -0,0 +1,9 @@
+//
+//  InvoiceCell.swift
+//  App2
+//
+//  Created by Tea Jakić on 16/09/2019.
+//  Copyright © 2019 Tea Jakić. All rights reserved.
+//
+
+import Foundation
---
+++ App2/SwiftUIView.swift
@@ -0,0 +1,21 @@
+//
+//  SwiftUIView.swift
+//  App2
+//
+//  Created by Tea Jakić on 01/10/2019.
+//  Copyright © 2019 Tea Jakić. All rights reserved.
+//
+
+import SwiftUI
+
+struct SwiftUIView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct SwiftUIView_Previews: PreviewProvider {
+    static var previews: some View {
+        SwiftUIView()
+    }
+}
